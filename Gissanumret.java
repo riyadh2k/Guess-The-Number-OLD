eee914a1ec56a88c3ea3982b133cabc5fa5abc4d
Syed Shabenur Hassan <112406985+riyadh2k@users.noreply.github.com>
2022-10-10 15:19:08 +0200

    Add files via upload

---
+++ Gissanumret.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+
+public class Gissanumret {
+
+
+	static  int getRandom() {
+		return 1 + (int)(100 * Math.random());
+	}
+	public static  void guessingNumberGame() {
+        // Scanner Class
+		Scanner sc = new Scanner(System.in);
+       
+       
+        int number = getRandom();
+        int tries = 0;
+        
+              
+        while(true) {
+          System.out.println("Enter your guess (1-100):");
+          
+          int Guess = sc.nextInt();
+          tries++;
+          
+          if (Guess == number) {
+            System.out.println("Correct! You Win!");
+            System.out.println("It took you " + tries + " tries");
+            System.out.println("---------------------------");
+            tries=0;
+            number = getRandom();          
+            
+          }
+          else if(number > Guess) {
+            System.out.println("Nope! The number is higher. Guess again.");
+          }
+          else {
+            System.out.println("Nope! The number is lower. Guess again.");
+          }
+          
+        } 
+	}
+ 
+
+    public static void main(String arg[])
+    {
+ 
+          guessingNumberGame();
+        
+    }
+}
+
